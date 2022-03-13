d2edba0efbbb110c975dd3825eedfd2fd117ea77
leont <leon.traykov@gmail.com>
2022-03-13 13:32:13 +0700

    icon of card suit added

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 # Программа для выбора случайной карты из игральной колоды
 values = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-suits = %w(Diamonds Hearts Spades Clubs)
+suits = ["\u2660", "\u2665", "\u2666", "\u2663"]
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample}#{suits.sample.encode('utf-8')}"
