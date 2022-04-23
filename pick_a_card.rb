19b827f03150bb8aa5e0a8929e376f71d69ef103
leont <leon.traykov@gmail.com>
2022-04-23 21:32:45 +0700

    Multiple cards

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -2,4 +2,19 @@
 values = %w(2 3 4 5 6 7 8 9 10 J Q K A)
 suits = ["\u2660", "\u2665", "\u2666", "\u2663"]
 
-puts "#{values.sample}#{suits.sample.encode('utf-8')}"
+cards = []
+
+values.each do |value|
+  suits.each do |suit|
+    cards << "#{value} #{suit}"
+  end
+end
+
+cards.shuffle!
+
+puts "How many cards do you pick?"
+number = $stdin.gets.to_i
+
+number.times do
+  puts cards.pop
+end
